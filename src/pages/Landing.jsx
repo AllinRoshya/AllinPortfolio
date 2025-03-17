bcec4b5602b657c8f893ae64d6388f9a339c927d
AllinRoshya <kookierosy1@gmail.com>
2025-03-17 15:06:25 +0530

    new

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -98,58 +98,58 @@ function Landing() {
             <section id="projects">
     <h2 className="name">Projects</h2>
     <div className="projects-wrapper">
-    <article className="project-card">
-    <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193226/employee_dbmggf.jpg" className="logo" />
-    <h3>Employee Management System <span className="tooltip">Java (OOPs)</span></h3>
-    <a href="https://github.com/AllinRoshya/EMS" target="_blank" rel="noopener noreferrer">
-        <button className="project-btn">View Project</button>
-    </a>
-</article>
+        
+        <a href="https://github.com/AllinRoshya/EMS" target="_blank" rel="noopener noreferrer" className="project-link">
+            <article className="project-card">
+                <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193226/employee_dbmggf.jpg" className="logo" />
+                <h3>Employee Management System</h3>
+                <span className="tooltip">Java (OOPs)</span>
+            </article>
+        </a>
 
-<article className="project-card">
-    <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193227/maxresdefault_ete0km.jpg" className="logo" />
-    <h3>Restaurant Management System <span className="tooltip">Data Structure</span></h3>
-    <a href="https://github.com/AllinRoshya/RMS" target="_blank" rel="noopener noreferrer">
-        <button className="project-btn">View Project</button>
-    </a>
-</article>
+        <a href="https://github.com/AllinRoshya/RMS" target="_blank" rel="noopener noreferrer" className="project-link">
+            <article className="project-card">
+                <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193227/maxresdefault_ete0km.jpg" className="logo" />
+                <h3>Restaurant Management System</h3> 
+                <span className="tooltip">Data Structure</span>
+            </article>
+        </a>
 
-<article className="project-card">
-    <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193227/star_fs6hv6.png" className="logo1" />
-    <h3>Quiz Maker <span className="tooltip">JavaScript</span></h3>
-    <a href="https://allink-5402-8443.zcodeusers.in/QUIZ%20MAKER/start.html" target="_blank" rel="noopener noreferrer">
-        <button className="project-btn">View Project</button>
-    </a>
-</article>
+        <a href="https://allink-5402-8443.zcodeusers.in/QUIZ%20MAKER/start.html" target="_blank" rel="noopener noreferrer" className="project-link">
+            <article className="project-card">
+                <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193227/star_fs6hv6.png" className="logo1" />
+                <h3>Quiz Maker</h3>
+                <span className="tooltip">JavaScript</span>
+            </article>
+        </a>
 
-<article className="project-card">
-    <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193227/logo_zlqqod.png" className="logo1" />
-    <h3>RCB <span className="tooltip">HTML/CSS</span></h3>
-    <a href="https://allink-7273-8443.zcodeusers.in/MYPROJECT/Home.html" target="_blank" rel="noopener noreferrer">
-        <button className="project-btn">View Project</button>
-    </a>
-</article>
+        <a href="https://allink-7273-8443.zcodeusers.in/MYPROJECT/Home.html" target="_blank" rel="noopener noreferrer" className="project-link">
+            <article className="project-card">
+                <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193227/logo_zlqqod.png" className="logo1" />
+                <h3>Royal Challengers Bangalore</h3>
+                <span className="tooltip">HTML/CSS</span>
+            </article>
+        </a>
 
-<article className="project-card">
-    <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193227/Git_Grove_ikbtq0.jpg" className="logo1" />
-    <h3>Git Grove <span className="tooltip">Java (Advanced)</span></h3>
-    <a href="https://github.com/rajkumarkvr/GitGrove_backend" target="_blank" rel="noopener noreferrer">
-        <button className="project-btn">View Project</button>
-    </a>
-</article>
+        <a href="https://github.com/rajkumarkvr/GitGrove_backend" target="_blank" rel="noopener noreferrer" className="project-link">
+            <article className="project-card">
+                <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193227/Git_Grove_ikbtq0.jpg" className="logo1" />
+                <h3>Git Grove</h3>
+                <span className="tooltip">Java (Advanced)</span>
+            </article>
+        </a>
 
-<article className="project-card">
-    <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193230/bm_guco3x.svg" className="logo" />
-    <h3>Scratch Project <span className="tooltip">Scratch</span></h3>
-    <a href="https://scratch.mit.edu/projects/1041087381" target="_blank" rel="noopener noreferrer">
-        <button className="project-btn">View Project</button>
-    </a>
-</article>
+        <a href="https://scratch.mit.edu/projects/1041087381" target="_blank" rel="noopener noreferrer" className="project-link">
+            <article className="project-card">
+                <img src="https://res.cloudinary.com/dcqy9shzk/image/upload/v1742193230/bm_guco3x.svg" className="logo" />
+                <h3>Scratch Project</h3>
+                <span className="tooltip">Scratch</span>
+            </article>
+        </a>
 
-
-</div>
-
+    </div>
 </section>
+
 
 <div className="footer">CORP ID: allin.k@zohocorp.com</div>
         </>
